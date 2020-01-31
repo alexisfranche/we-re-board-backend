3e5872a2d604258e2519a52c7d6599c768d8b2da
Alexis Franche <alexis.franche@mail.mcgill.ca>
2020-01-30 22:27:46 -0500

    init flask and deploy

---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask, request , jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+
+# Init app
+app = Flask(__name__)
+
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://ozqhfdunsqxrnj:758183f57a6468bbfd5f6f4f99c6a753f1e3a2afae37699c8922ca520a488bd3@ec2-52-203-98-126.compute-1.amazonaws.com:5432/d674iu1eqcu3l9"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Init db
+db = SQLAlchemy(app)
+
+# Init Marshmallow
+ma = Marshmallow(app)
+
+# User Class/Model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=False)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(200))
+
+    def __init__(self, name, email, password):
+        self.name=name
+        self.email=email
+        self.password=password
+
+# User Schema
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'email', 'password')
+
+# Init Schema User
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+# Create a Product
+@app.route('/user', methods=['POST'])
+def add_user():
+    name = request.json['name']
+    email = request.json['email']
+    password = request.json['password']
+
+    new_user = User(name, email, password)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return user_schema.jsonify(new_user)
+
+# Get all users
+@app.route('/user', methods=['GET'])
+def get_users():
+    all_users = User.query.all()
+    result = users_schema.dump(all_users)
+    return jsonify(result)
+
+db.create_all()
+
+# Run Server 
+if __name__ == '__main__':
+    app.run(debug=True)
